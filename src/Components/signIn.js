0e8fd4f67affb6d49eb81892d575da77f1e70323
martcamila1 <martcamila1@gmail.com>
2020-03-10 10:16:05 -0400

    css signIn y agrego componentes al signin

--- src/Components/signIn.js
+++ src/Components/signIn.js
@@ -1,6 +1,7 @@
 //login de app
 import React, { useState } from 'react'
 import {auth} from '../firebase-config/firebaseInit'
+import './signIn.css'
 import {Link} from 'react-router-dom'
 
 const SignIn = ()=>{
@@ -10,7 +11,7 @@ const SignIn = ()=>{
   let sendFirebase =() =>{
     auth.signInWithEmailAndPassword(email, password)
     .then(function(){
-      alert("chaolin")
+      alert("chao")
     })
     
     .catch(function(error) {
@@ -23,10 +24,32 @@ const SignIn = ()=>{
     });
   }
   return(
-    <div>
-      <input className="inputSign" type="email" placeholder="Ingresa tu correo" onChange={(event)=>setEmail(event.target.value)}/>
+    <div className="containerSignIn">
+      <div>
+        <div className="iniSignIn">
+          <div className="">
+          <p className="SignIn">Iniciar sesión</p>
+          <p className="textSignIn">Accede a tu cuente y paga veloz</p>
+          <Link to="/"><button className="buttonSignIn2">Ir a mi cuenta</button></Link>
+          </div>
+        </div>
+        <div className="iniSignIn">
+        <div className="">
+          <p className="SignIn">Iniciar sesión</p>
+          <p className="textSignIn">Accede a tu cuente y paga veloz</p>
+          <button className="buttonSignIn2">Ir a mi cuenta</button>
+          </div>
+          
+        </div>
+      </div>
+      <div className="imgRegistration">
+
+      </div>
+      <div className="containerInputSignIn">
+      <input id="inputSign1"className="inputSign" type="email" placeholder="Ingresa tu correo" onChange={(event)=>setEmail(event.target.value)}/>
       <input className="inputSign"type="password"placeholder="Pin" onChange={(event)=>setPassword(event.target.value)}/>
-      <Link to="/Feed"><button onClick={sendFirebase}>Ingresar</button></Link>
+      <Link to="/Feed"><button className="buttonSignIn" onClick={sendFirebase}>Ingresar</button></Link>
+    </div>
     </div>
   )
 }
